d743d5f50b33536f5f863f995c74f482854247c7
Trikyas <mooneychad@yahoo.com>
2018-10-08 23:49:52 +1100

    token buying
    …

--- Chest/contracts/CryptoPirateTokenSale.sol
+++ Chest/contracts/CryptoPirateTokenSale.sol
@@ -6,10 +6,14 @@ contract CryptoPirateTokenSale {
   address admin;
   CryptoPirateToken public tokenContract;
   uint256 public tokenPrice;
+  uint256 public tokensSold;
 
   function CryptoPirateTokenSale(CryptoPirateToken _tokenContract, uint256 _tokenPrice) public {
     admin = msg.sender;
     tokenContract = _tokenContract;
     tokenPrice = _tokenPrice;
+  }
+  function buyTokens(uint256 _numberOfTokens) public payable {
+    tokensSold += _numberOfTokens;
   }
 }
--- Chest/test/CryptoPirateTokenSale.js
+++ Chest/test/CryptoPirateTokenSale.js
@@ -2,7 +2,9 @@ var CryptoPirateTokenSale = artifacts.require("./CryptoPirateTokenSale.sol");
 
 contract('CryptoPirateTokenSale', function(accounts) {
   var tokenSaleInstance;
+  var buyer = accounts[1];
   var tokenPrice = 1000000000000000;  // This is in WEI
+  var numberOfTokens;
   it('initializes the contract with the correct values', function() {
     return CryptoPirateTokenSale.deployed().then(function(instance) {
       tokenSaleInstance = instance;
@@ -17,6 +19,16 @@ contract('CryptoPirateTokenSale', function(accounts) {
       assert.equal(price, tokenPrice, 'token price is correct');
     });
   });
-
+  it('facilitates token buying', function() {
+    return CryptoPirateTokenSale.deployed().then(function(instance) {
+      tokenSaleInstance = instance;
+      numberOfTokens = 10;
+      return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
+    }).then(function(receipt) {
+      return tokenSaleInstance.tokensSold();
+    }).then(function(amount) {
+      assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+    });
+  });
 
 });
