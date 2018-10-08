8acdd537abbf524d9c846bfd67c5a0b25f2a8946
Trikyas <mooneychad@yahoo.com>
2018-10-08 20:51:39 +1100

    Pirate
    …

--- Chest/contracts/CryptoPirateToken.sol
+++ Chest/contracts/CryptoPirateToken.sol
@@ -39,6 +39,9 @@ contract CryptoPirateToken {
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
     require(_value <= balanceOf[_from]);
     require(_value <= allowance[_from][msg.sender]);
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    allowance[_from][msg.sender] -= _value;
     Transfer(_from, _to, _value);
     return true;
   }
