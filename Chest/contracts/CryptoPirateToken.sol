ba709e7d10abbf61c8e0a4c3008cab0404bda3d2
Trikyas <mooneychad@yahoo.com>
2018-10-08 20:31:59 +1100

    Arrr!
    …

--- Chest/contracts/CryptoPirateToken.sol
+++ Chest/contracts/CryptoPirateToken.sol
@@ -11,8 +11,14 @@ contract CryptoPirateToken {
     address indexed _to,
     uint256 _value
   );
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
 // Map for Keys and Values
   mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   function CryptoPirateToken(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -23,6 +29,17 @@ contract CryptoPirateToken {
     balanceOf[msg.sender] -= _value;
     balanceOf[ _to ] += _value;
     Transfer(msg.sender, _to, _value);
+    return true;
+  }
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+    Transfer(_from, _to, _value);
     return true;
   }
 }
--- Chest/test/CryptoPirateToken.js
+++ Chest/test/CryptoPirateToken.js
@@ -52,5 +52,51 @@ it('transfers token ownership', function() {
     assert.equal(balance.toNumber(), 750000, 'deducts amount from the sending account');
   });
 });
-
+it('approves tokens for delegated transfer', function() {
+  return CryptoPirateToken.deployed().then(function(instance) {
+    tokenInstance = instance;
+    return tokenInstance.approve.call(accounts[1], 100);
+  }).then(function(success) {
+    assert.equal(success, true, 'it returns true');
+    return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+  }).then(function(receipt) {
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Approval', 'Is the approval event');
+    assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+    assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+    assert.equal(receipt.logs[0].args._value, 100, 'logs the transferred amount');
+    return tokenInstance.allowance(accounts[0], accounts[1]);
+  }).then(function(allowance) {
+    assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+  });
+});
+it('handles delegated token transfers', function() {
+  return CryptoPirateToken.deployed().then(function(instance) {
+    tokenInstance = instance;
+    fromAccount = accounts[2];
+    toAccount = accounts[3];
+    spendingAccount = accounts[4];
+    return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+  }).then(function(receipt) {
+    return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+  }).then(function(receipt) {
+    return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+  }).then(assert.fail).catch(function(error) {
+    assert(error.message.indexOf('revert') >= 0, 'cannot transfer valuer larger than balance');
+    return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+  }).then(assert.fail).catch(function(error) {
+    assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
+    return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+  }).then(function(success) {
+    assert.equal(success, true);
+    return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+  }).then(function(receipt) {
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'Is the "Transfer" event');
+    assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+    assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+    assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+  });
+});
+//END TEST
 });
