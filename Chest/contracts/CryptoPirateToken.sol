464b3c22683c935ef03bbbb36ddbcebb46477405
Trikyas <mooneychad@yahoo.com>
2018-10-08 17:02:58 +1100

    bug

--- Chest/contracts/CryptoPirateToken.sol
+++ Chest/contracts/CryptoPirateToken.sol
@@ -5,16 +5,24 @@ contract CryptoPirateToken {
   string public symbol = "CPT";
   string public standard = "CryptoPirate v1.0";
   uint256 public totalSupply;
+
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
 // Map for Keys and Values
   mapping(address => uint256) public balanceOf;
 
-  function CryptoPirateToken (uint256 _initialSupply) public {
+  function CryptoPirateToken(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
   }
   function transfer( address _to, uint256 _value) public returns (bool success) {
     require(balanceOf[msg.sender] >= _value);
     balanceOf[msg.sender] -= _value;
-    balanceOf[_to] += _value;
+    balanceOf[ _to ] += _value;
+    Transfer(msg.sender, _to, _value);
+    return true;
   }
 }
--- Chest/test/CryptoPirateToken.js
+++ Chest/test/CryptoPirateToken.js
@@ -15,7 +15,7 @@ contract('CryptoPirateToken', function(accounts) {
     }).then(function(standard) {
       assert.equal(standard, 'CryptoPirate v1.0', 'has correct standard');
     });
-  })
+  });
   it('allocates the initial supply upon deployment', function() {
     return CryptoPirateToken.deployed().then(function(instance) {
       tokenInstance = instance;
@@ -31,16 +31,24 @@ contract('CryptoPirateToken', function(accounts) {
 it('transfers token ownership', function() {
   return CryptoPirateToken.deployed().then(function(instance) {
     tokenInstance = instance;
-    return tokenInstance.transfer.call(accounts[1], 999999999999999999999);
+    return tokenInstance.transfer.call(accounts[1], 9999999999999);
   }).then(assert.fail).catch(function(error) {
     assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
     return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+  }).then(function(success) {
+    assert.equal(success, true, 'it returns true');
+    return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
   }).then(function(receipt) {
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'Is the transfer event');
+    assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+    assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+    assert.equal(receipt.logs[0].args._value, 250000, 'logs the transferred amount');
     return tokenInstance.balanceOf(accounts[1]);
   }).then(function(balance) {
     assert.equal(balance.toNumber(), 250000, 'adds the amount to the recieving account');
     return tokenInstance.balanceOf(accounts[0]);
   }).then(function(balance) {
-    assert.equal(balance.toNumber(), 750000, 'deducts amount from account');
+    assert.equal(balance.toNumber(), 750000, 'deducts amount from the sending account');
   });
 });
